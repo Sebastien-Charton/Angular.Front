c7c764a5576fd80bf9c79e8fed73a0f2302cbc61
Sebastien Charton <sebastiencharton@protonmail.com>
2024-01-06 17:50:17 +0200

    environment added + retrieve url from env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {provideHttpClient} from "@angular/common/http";
+import {AUTH_API_URL} from "../_clients/web-api-client";
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    { provide: AUTH_API_URL, useValue: environment.authApiUrl }]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  appName: 'Angular.Front',
+  production: false,
+  authApiUrl: 'http://localhost:5200'
+};
