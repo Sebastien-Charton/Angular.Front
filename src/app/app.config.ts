142b78dd988ed950d07b4df490234530c4ab9263
Sebastien Charton <sebastiencharton@protonmail.com>
2024-01-07 21:05:44 +0200

    interceptor added to send bearer token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {AUTH_API_URL} from "../_clients/web-api-client";
 import {environment} from "../environments/environment";
 import {JWT_OPTIONS, JwtHelperService} from "@auth0/angular-jwt";
+import {authInterceptor} from "../_interceptors/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     { provide: AUTH_API_URL, useValue: environment.authApiUrl },
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService]
--- src/app/pages/message/message.component.ts
+++ src/app/pages/message/message.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TokenService} from "../../../_services/token.service";
+import {AuthService} from "../../../_services/auth.service";
 
 @Component({
   selector: 'app-message',
@@ -9,15 +10,26 @@ import {TokenService} from "../../../_services/token.service";
   styleUrl: './message.component.scss'
 })
 export class MessageComponent {
-
   public token: string | null = null;
-  constructor(tokenService: TokenService) {
+  constructor(tokenService: TokenService, private authService: AuthService) {
     this.token = tokenService.getToken()
     console.log(tokenService.decodeToken())
     console.log(tokenService.getUserName())
     console.log(tokenService.getRoles())
     console.log(tokenService.getEmail())
     console.log(tokenService.getUserId())
+
+    this.authService.isEmailConfirmed().subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => {
+        console.log(error)
+      },
+      complete: () => {
+        console.log('complete')
+      }
+    })
   }
 
 }
